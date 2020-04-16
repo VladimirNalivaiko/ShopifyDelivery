d44475f420e6baac9655be77f980e13003c4676a
Vladimir Nalivaiko <nalivaikovova@gmail.com>
2020-04-16 17:10:25 +0300

    Identity added

--- ShopifyDelivery/Startup.cs
+++ ShopifyDelivery/Startup.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ShopifyDelivery.BLL.Model;
+using ShopifyDelivery.DAL;
+using ShopifyDelivery.DAL.Infrastructure;
 
 namespace ShopifyDelivery
 {
@@ -20,6 +25,20 @@ namespace ShopifyDelivery
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.Configure<DbSettings>(options =>
+            //{
+            //    options.ConnectionString
+            //        = Configuration.GetSection("SqlConnection:ConnectionString").Value;
+            //    options.Database
+            //        = Configuration.GetSection("SqlConnection:Database").Value;
+            //});
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("LocalConnection")));
+
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
