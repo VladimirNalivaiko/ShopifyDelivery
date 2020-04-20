0e409aa570a529b28a3ccae11b66cb9f9d07ddae
Vladimir Nalivaiko <nalivaikovova@gmail.com>
2020-04-20 18:15:18 +0300

    managers update

---
+++ ShopifyDelivery/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using ShopifyDelivery.BLL.Managers.Implementation;
+using ShopifyDelivery.BLL.Model;
+
+namespace ShopifyDelivery.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : BaseApiController
+    {
+        private readonly ApplicationUserManager<ApplicationUser> _userManager;
+        private readonly ApplicationSignInManager<ApplicationUser> _signInManager;
+        public IConfiguration Configuration { get; }
+
+        public AccountController(IConfiguration configuration,
+            ApplicationUserManager<ApplicationUser> userManager,
+            ApplicationSignInManager<ApplicationUser> signInManager)
+        {
+            Configuration = configuration;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+    }
+}
---
+++ ShopifyDelivery/Controllers/BaseApiController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopifyDelivery.Controllers
+{
+    public class BaseApiController : ControllerBase
+    {
+        public string UserId => User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+    }
+}
